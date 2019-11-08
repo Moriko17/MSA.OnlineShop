acbee4ad0c4db8554f5d58fbdcec1b7ffd0b915e
Moriko17 <17moriko@gmail.com>
2019-11-08 10:00:19 +0300

    done with order

--- OrderService/src/main/java/com/mc/order/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/mc/order/OrderService/controller/OrderController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-//@RequestMapping("/orders")
 public class OrderController {
     private OrderService orderService;
     @Autowired
