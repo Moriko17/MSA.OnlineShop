92d79730466f6438b46c570733af0e8e7c12d237
Moriko17 <17moriko@gmail.com>
2019-11-16 21:52:55 +0300

    q f

--- OrderService/src/main/java/com/mc/order/OrderService/controller/OrderController.java
+++ OrderService/src/main/java/com/mc/order/OrderService/controller/OrderController.java
@@ -6,7 +6,6 @@ import com.mc.order.OrderService.domain.OrderStatus;
 import com.mc.order.OrderService.service.OrderService;
 import com.mc.warehouse.api.client.WarehouseServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,7 +34,6 @@ public class OrderController {
     @PostMapping("/{id}/item")
     public OrderDto addItemToOrder(@PathVariable String id, @RequestBody ItemDto itemDto) {
         com.mc.warehouse.api.models.ItemDto itemDto1 = warehouseServiceClient.getItemById(Long.parseLong(id));
-//        warehouseServiceClient.getItemById(Long.parseLong(id));
         return orderService.addItemToOrder(id, itemDto);
     }
 
